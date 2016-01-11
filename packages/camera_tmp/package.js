49a32ffe1c3faf5c072eae6537fe9635910c1aa4
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2016-01-11 15:58:03 -0200

    teste

---
+++ packages/camera_tmp/package.js
@@ -0,0 +1,28 @@
+Package.describe({
+  name: 'meteoric:camera',
+  summary: 'Camera with one function call on desktop and mobile.',
+  version: '1.0.2',
+});
+
+Cordova.depends({
+  'org.apache.cordova.camera':'0.3.2'
+});
+
+Package.onUse(function(api) {
+  api.export('MeteoricCamera');
+  api.use(['templating', 'session', 'ui', 'blaze', 'less@2.5.0_2', 'reactive-var', 'meteoric:ionic@0.1.13']);
+  api.versionsFrom('METEOR@1.0');
+
+  api.addFiles('camera.js');
+
+  api.addFiles([
+    'camera-browser.js',
+    'camera.less',
+    'templates/errorMessage.html',
+    'templates/permissionDenied.html',
+    'templates/viewfinder.html',
+    'templates/viewfinder.js'
+  ], ['web.browser']);
+
+  api.addFiles('camera-cordova.js', ['web.cordova']);
+});
