b307fc7da1933a5eb9132dd7edff60b6dcc2acef
Fred Fernandes <Frefer@MacBook-Pro-de-Fred.local>
2016-01-16 15:17:13 -0200

    fixed navbar extra space

--- client/config/header_fix.js
+++ client/config/header_fix.js
@@ -1,14 +1,6 @@
-Template.defaultLayout.events({
-	'click .back-button-especial':function(){
-		Router.go('home');
-	}
-})
-
-Template.defautLayout.onRendered(function() {
 if (Meteor.isCordova) {
   document.addEventListener("deviceready", function() {
     StatusBar.overlaysWebView(true);
     StatusBar.styleLightContent();
   }, false);
 }
-});
--- mobile-config.js
+++ mobile-config.js
@@ -1,1 +1,13 @@
 App.accessRule('*');
+
+App.info({
+  id: 'com.shact.app',
+  name: 'shact',
+  description: 'Conecte-se',
+  author: 'Rafael R. Correia',
+  email: 'rafael.correia.poli@gmail.com',
+  website: 'http://example.com'
+});
+
+App.setPreference('StatusBarOverlaysWebView', true);
+App.setPreference('StatusBarStyle', 'default');
